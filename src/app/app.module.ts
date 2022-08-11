b1eb1e579e1c8529f237457ff04cfce48b5f2b21
qadriusa <qadri.usa750@gmail.com>
2022-08-11 16:13:26 +0500

    Angular Business First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './MyComponents/info/info.component';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { InterestsComponent } from './MyComponents/interests/interests.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    AboutComponent,
+    InterestsComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
